43e875246d7e8b3cbca8bcfeae69bb7259af4967
Laura Jackson <playeronereadystart@gmail.com>
2019-01-04 11:59:07 +0000

    Changes to App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BadgeList from './components/BadgeList/BadgeList'
+import BadgeJson from './assets/badge.json';
+//svgs
+import seaSvg from './assets/svg/sea.svg';
+import speakSvg from './assets/svg/speak.svg';
+import socialSvg from './assets/svg/social.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { badges: [speakSvg, seaSvg, socialSvg]}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <BadgeList badges={this.state.badges} badgeInfo={BadgeJson}/>
+
         </header>
       </div>
     );
