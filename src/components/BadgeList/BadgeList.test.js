24769045b122645a214569b9e226a9377c3859ab
Laura Jackson <playeronereadystart@gmail.com>
2019-01-04 12:29:36 +0000

    Fix test

--- src/components/BadgeList/BadgeList.test.js
+++ src/components/BadgeList/BadgeList.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BadgeList from './BadgeList';
+import BadgeJson from '../../assets/badge.json'
 
 it('renders BadgeList without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BadgeList />, div);
+  ReactDOM.render(<BadgeList
+    badges={["mockImg1", "mockImg2", "mockImg3"]}
+    badgeInfo={BadgeJson} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
