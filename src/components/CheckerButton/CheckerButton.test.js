24145fd40876efe5e1b556f54076f4be4cb88cc7
Laura Jackson <playeronereadystart@gmail.com>
2019-01-09 16:47:03 +0000

    change checker button test to be less brittle

--- src/components/CheckerButton/CheckerButton.test.js
+++ src/components/CheckerButton/CheckerButton.test.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CheckerButton from './CheckerButton';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-it('renders BadgeList without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CheckerButton
-    handleCheck={function() {}} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { handleCheck: () => {  } };
+
+const setup = (props={}) => {
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<CheckerButton {...setupProps} />);
+}
+
+const findByTestAttr = (wrapper, val, selector ='data-test') => {
+  return wrapper.find(`[${selector}='${val}']`);
+};
+
+test('renders button without crashing, initally displays ` o `', () => {
+  const wrapper = setup();
+  expect(wrapper.text()).toContain('o');
+});
+
+test('displays a check when button is clicked', () => {
+  //to do, spy on
 });
