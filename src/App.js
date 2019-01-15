0193df67c746058d1953c6bb9a470d733dfebb3e
Laura Jackson <playeronereadystart@gmail.com>
2019-01-15 12:23:42 +0000

    include IndividualChallenge with dummy data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import BadgeList from './components/BadgeList/BadgeList';
-import BadgeJson from './assets/badge.json';
+import challengeJson from './assets/challenges.json';
+import IndividualChallenge from './components/IndividualChallenge/IndividualChallenge';
 //svgs
-import seaSvg from './assets/svg/sea.svg';
-import speakSvg from './assets/svg/speak.svg';
-import socialSvg from './assets/svg/social.svg';
 
 import './App.css';
-
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { badges: [speakSvg, seaSvg, socialSvg]}
-  }
+    super();
 
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <BadgeList badges={this.state.badges} badgeInfo={BadgeJson}/>
-        </header>
+        <IndividualChallenge challenge='Get swimming'
+          success={true} />
       </div>
     );
   }
